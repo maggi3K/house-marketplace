30a9fab83ab9e81c842111148051f7e41d88a8f5
Magi Karatasevska <magi.karatasevska@gmail.com>
2022-08-18 13:59:42 +0200

    changes

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from 'react'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 import Spinner from '../components/Spinner'
 
 function CreateListing() {
@@ -42,6 +43,8 @@ function CreateListing() {
     const navigate = useNavigate()
     const isMounted = useRef(true)
 
+   
+
     useEffect(() => {
         if(isMounted) {
             onAuthStateChanged(auth, (user) => {
@@ -58,12 +61,83 @@ function CreateListing() {
         }
     }, [isMounted])
 
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault()
+
+        setLoading(true)
+
+        if(+discountedPrice >= +regularPrice) {
+            setLoading(false)
+            toast.error('Discounted price needs to be less than regular price')
+
+            return
+        }
+
+        if (images.length > 6) {
+            setLoading(false)
+            toast.error('Max 6 images')
+            return
+        }
+
+    
+        let geolocation = {}
+        let location
+
+        if(geolocationEnabled) {
+
+            const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`)
+
+            const data = await response.json()
+
+            geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
+
+            geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
+            
+            location = data.status === 'ZERO_RESULTS' ? undefined : data.results[0]?.formatted_address
+
+            if(location === undefined || location.includes('undefined')){
+                setLoading(false)
+                toast.error('Please enter a correct address')
+                return
+            }
+            
+        } else {
+            geolocation.lat = latitude
+            geolocation.lng = longitude
+            location = address
+
+        
+        }
+
+        setLoading(false)
     }
 
     const onMutate = (e) => {
         let boolean = null
+
+        if(e.target.value === 'true') {
+            boolean = true
+        }
+        if(e.target.value === 'false') {
+            boolean = false
+        }
+
+        //Files
+        if(e.target.files) {
+            setFormData((prevState) => ({
+                ...prevState,
+                images: e.target.files
+            }))
+        }
+
+        //Text/Booleans/Numbers
+
+        if(!e.target.files) {
+            setFormData((prevState) => ({
+                ...prevState,
+                [e.target.id]: boolean ?? e.target.value
+            }))
+        }
     }
 
     if(loading) {
