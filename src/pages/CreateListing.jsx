e1843d4716e22abf6d3f52cec82f1f933672d780
Magi Karatasevska <magi.karatasevska@gmail.com>
2022-08-25 17:30:36 +0200

    added leaflet map

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,7 +1,11 @@
 import { useState, useEffect, useRef } from 'react'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage"
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebase.config'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import {v4 as uuidv4} from 'uuid'
 import Spinner from '../components/Spinner'
 
 function CreateListing() {
@@ -104,12 +108,81 @@ function CreateListing() {
         } else {
             geolocation.lat = latitude
             geolocation.lng = longitude
-            location = address
+            
 
         
         }
+        // Store images in firebase
+        const storeImage = async (image) => {
+            return new Promise((resolve, reject) => {
+                const storage = getStorage()
+                const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
 
+                const storageRef = ref(storage, 'images/' + fileName)
+
+                const uploadTask = uploadBytesResumable(storageRef, image);
+
+                uploadTask.on(
+                  "state_changed",
+                  (snapshot) => {
+                    
+                    const progress =
+                      (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log("Upload is " + progress + "% done");
+                    switch (snapshot.state) {
+                      case "paused":
+                        console.log("Upload is paused");
+                        break;
+                      case "running":
+                        console.log("Upload is running");
+                        break;
+                    }
+                  },
+                  (error) => {
+                    reject(error)
+                  },
+                  () => {
+                    // Handle successful uploads on complete
+                    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                    getDownloadURL(uploadTask.snapshot.ref).then(
+                      (downloadURL) => {
+                        resolve(downloadURL);
+                      }
+                    );
+                  }
+                );
+            })
+        }
+
+        const imgUrl = await Promise.all(
+            [...images].map((image) => storeImage(image))
+        ).catch(() => {
+            setLoading(false)
+            toast.error('Images not uploaded')
+            return
+        })
+
+        const formDataCopy = {
+          ...formData,
+          imgUrl,
+          geolocation,
+          timestamp: serverTimestamp()
+        }
+
+        formDataCopy.location = address
+
+        delete formDataCopy.images
+        delete formDataCopy.address
+
+
+        !formDataCopy.offer && delete formDataCopy.discountedPrice
+
+        const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
+        
         setLoading(false)
+
+        toast.success('Listing saved')
+        navigate(`/category/${formDataCopy.type}/${docRef.id}`)
     }
 
     const onMutate = (e) => {
@@ -359,7 +432,7 @@ function CreateListing() {
 
           <label className='formLabel'>Images</label>
           <p className='imagesInfo'>
-            The first image will be the cover (max 6).
+            The first image will be the cover (max 6) and must be less than 2MB in size.
           </p>
           <input
             className='formInputFile'
@@ -367,7 +440,7 @@ function CreateListing() {
             id='images'
             onChange={onMutate}
             max='6'
-            accept='.jpg,.png,.jpeg'
+            accept='.jpg,.png,.jpeg,.avif,.webp'
             multiple
             required
           />
