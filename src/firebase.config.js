1b2e3e99834ae08f621c058a4155aa0320a7161b
Magi Karatasevska <magi.karatasevska@gmail.com>
2023-08-03 14:54:47 +0200

    added new favicon

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,6 +1,6 @@
 
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore';
 
 
 // Your web app's Firebase configuration
