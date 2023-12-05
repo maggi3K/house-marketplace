c98e0c43ea7416ceeeba9276ccef259c0659f3eb
Magi Karatasevska <magi.karatasevska@gmail.com>
2023-12-05 16:46:20 +0100

    css changes

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -3,36 +3,51 @@ import Slider from '../components/Slider'
 import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
 import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
 
+
 function Explore() {
   return (
-    <div className='explore'>
-        <header>
-          <p className="pageHeader">Explore</p>
+    <>
+      
+          <header>
+            <div className='container'>
+              <div className='heroContainer'>
+                <div className='letsExplore'>
+                  Easiest way to find your dream house
+                </div>
+              </div> 
+              </div>
           </header>
+        
+            <div className='explore'>
 
-          <main>
-          <Slider />
+              <main>
+                <Slider />
 
-            <p className="exploreCategoryHeading">Categories</p>
+                <p className="exploreCategoryHeading">Explore categories</p>
 
-            <div className="exploreCategories">
-              <Link to='/category/rent'>
-                <img src={rentCategoryImage} 
-                alt="rent"
-                className='exploreCategoryImg' />
+                <div className="exploreCategories">
+                  
+                  <Link to='/category/rent'>
+                    <img src={rentCategoryImage} 
+                    alt="rent"
+                    className='exploreCategoryImg' />
 
-                <p className="exploreCategoryName">Places for rent</p>
-              </Link>
-              <Link to='/category/sale'>
-                <img src={sellCategoryImage} 
-                alt="sell"
-                className='exploreCategoryImg' />
+                    <p className="exploreCategoryName">Places for rent</p>
+                  </Link>
 
-                <p className="exploreCategoryName">Places for sale</p>
-              </Link>
+                  <Link to='/category/sale'>
+                    <img src={sellCategoryImage} 
+                    alt="sell"
+                    className='exploreCategoryImg' />
+
+                    <p className="exploreCategoryName">Places for sale</p>
+                  </Link>
+
+                </div>
+
+              </main>
             </div>
-          </main>
-    </div>
+    </>
   )
 }
 
